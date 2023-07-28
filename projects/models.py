827ad8f9094ddabb8bab8dc38ef707f539b48cae
Hadiz <adefemihadizi2020@gmail.com>
2023-07-28 21:28:39 +0100

    ade few changes

--- projects/models.py
+++ projects/models.py
@@ -5,7 +5,6 @@ class Project(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
     technology = models.CharField(max_length=20)
-    image = models.ImageField(upload_to='project_images', blank=True, null=True)
     #uploaded_at = models.DateTimeField(auto_now_add=True)
     
     
